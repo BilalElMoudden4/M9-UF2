f65bd6a412b2b7670702669efcd2ff3ab8fc34bc
Bilal El Moudden <belel22@jaumebalmes.net>
2024-05-30 11:50:23 +0200

    ahora el disco gira mientras se reproduce la cancion

--- script.js
+++ script.js
@@ -1,67 +1,73 @@
-const albumImg = document.getElementById('album-img');
-const songTitle = document.getElementById('song-title');
-const artist = document.getElementById('artist');
+const imgAlbum = document.getElementById('album-img');
+const tituloCancion = document.getElementById('song-title');
+const artista = document.getElementById('artist');
 const audio = document.getElementById('audio');
-const playPauseBtn = document.getElementById('playPause');
-const nextBtn = document.getElementById('next');
-const prevBtn = document.getElementById('prev');
-const progressSlider = document.getElementById('progress');
+const botonPlayPausa = document.getElementById('playPause');
+const botonSiguiente = document.getElementById('next');
+const botonAnterior = document.getElementById('prev');
+const barraProgreso = document.getElementById('progress');
 
-let currentIndex = 0;
-let songs = [];
+let indiceActual = 0;
+let canciones = [];
 
 fetch('songs.json')
     .then(response => response.json())
     .then(data => {
-        songs = data.songs;
-        loadSong(currentIndex);
+        canciones = data.songs;
+        cargarCancion(indiceActual);
     });
 
-function loadSong(index) {
-    const currentSong = songs[index];
-    albumImg.src = currentSong.img;
-    songTitle.innerText = currentSong.name;
-    artist.innerText = currentSong.artist;
-    audio.src = currentSong.path;
+function cargarCancion(indice) {
+    const cancionActual = canciones[indice];
+    imgAlbum.src = cancionActual.img;
+    tituloCancion.innerText = cancionActual.name;
+    artista.innerText = cancionActual.artist;
+    audio.src = cancionActual.path;
 }
 
-function playPause() {
+function reproducirPausar() {
     if (audio.paused) {
         audio.play();
-        playPauseBtn.innerHTML = '<span class="material-icons">pause</span>';
+        botonPlayPausa.innerHTML = '<span class="material-icons">pause</span>';
+        imgAlbum.classList.add('rotating');
     } else {
         audio.pause();
-        playPauseBtn.innerHTML = '<span class="material-icons">play_arrow</span>';
+        botonPlayPausa.innerHTML = '<span class="material-icons">play_arrow</span>';
+        imgAlbum.classList.remove('rotating');
     }
 }
 
-function nextSong() {
-    currentIndex = (currentIndex + 1) % songs.length;
-    loadSong(currentIndex);
+function siguienteCancion() {
+    indiceActual = (indiceActual + 1) % canciones.length;
+    cargarCancion(indiceActual);
     audio.pause();
-    playPauseBtn.innerHTML = '<span class="material-icons">play_arrow</span>';
+    botonPlayPausa.innerHTML = '<span class="material-icons">play_arrow</span>';
+    imgAlbum.classList.remove('rotating');
 }
 
-function prevSong() {
-    currentIndex = (currentIndex - 1 + songs.length) % songs.length;
-    loadSong(currentIndex);
+function anteriorCancion() {
+    indiceActual = (indiceActual - 1 + canciones.length) % canciones.length;
+    cargarCancion(indiceActual);
     audio.pause();
-    playPauseBtn.innerHTML = '<span class="material-icons">play_arrow</span>';
+    botonPlayPausa.innerHTML = '<span class="material-icons">play_arrow</span>';
+    imgAlbum.classList.remove('rotating');
 }
 
-function updateProgress() {
-    const progress = (audio.currentTime / audio.duration) * 100;
-    progressSlider.value = progress;
-    progressSlider.style.setProperty('--value', progress);
+function actualizarProgreso() {
+    const progreso = (audio.currentTime / audio.duration) * 100;
+    barraProgreso.value = progreso;
+    barraProgreso.style.setProperty('--value', progreso);
 }
 
-function changeProgress() {
-    const progress = (progressSlider.value / 100) * audio.duration;
-    audio.currentTime = progress;
+function cambiarProgreso() {
+    const progreso = (barraProgreso.value / 100) * audio.duration;
+    audio.currentTime = progreso;
 }
 
-playPauseBtn.addEventListener('click', playPause);
-nextBtn.addEventListener('click', nextSong);
-prevBtn.addEventListener('click', prevSong);
-progressSlider.addEventListener('input', changeProgress);
-audio.addEventListener('timeupdate', updateProgress);
+botonPlayPausa.addEventListener('click', reproducirPausar);
+botonSiguiente.addEventListener('click', siguienteCancion);
+botonAnterior.addEventListener('click', anteriorCancion);
+barraProgreso.addEventListener('input', cambiarProgreso);
+audio.addEventListener('timeupdate', actualizarProgreso);
+audio.addEventListener('play', () => imgAlbum.classList.add('rotating'));
+audio.addEventListener('pause', () => imgAlbum.classList.remove('rotating'));
